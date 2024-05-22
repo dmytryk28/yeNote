e1ddd5bf5ede528f9abfa7fd09e3764f21224248
Dmytro Shvets <d.shvets@ukma.edu.ua>
2024-05-22 20:23:39 +0300

    add piano sustain

--- website/static/js/piano.js
+++ website/static/js/piano.js
@@ -1,13 +1,53 @@
-function playSound(note) {
-    let audio = new Audio('static/notes/' + note + '.wav');
-    audio.play();
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const keys = document.querySelectorAll(".key");
+    const sustainCheckbox = document.getElementById("sustain");
+    const minPlayTime = 500; // Мінімальна тривалість звучання в мілісекундах
 
-// Додавання обробника подій до клавіш
-var keys = document.querySelectorAll('.key');
-keys.forEach(function (key) {
-    key.addEventListener('click', function () {
-        let note = this.getAttribute('data-note');
-        playSound(note);
+    keys.forEach(key => {
+        const note = key.getAttribute("data-note");
+        let audio = new Audio(`static/notes/${note}.wav`);
+        let playStartTime = 0;
+
+        key.addEventListener("mousedown", () => {
+            playStartTime = Date.now();
+            playNoteWithSustain(audio);
+        });
+        key.addEventListener("mouseup", () => {
+            const elapsedTime = audio.currentTime - playStartTime;
+            if (elapsedTime >= minPlayTime) {
+                stopNoteWithoutSustain(audio);
+            } else {
+                setTimeout(() => stopNoteWithoutSustain(audio), minPlayTime - elapsedTime);
+            }
+        });
+        key.addEventListener("mouseleave", () => {
+            const elapsedTime = audio.currentTime - playStartTime;
+            if (elapsedTime >= minPlayTime) {
+                stopNoteWithoutSustain(audio);
+            } else {
+                setTimeout(() => stopNoteWithoutSustain(audio), minPlayTime - elapsedTime);
+            }
+        });
     });
+
+    function playNoteWithSustain(audio) {
+        audio.currentTime = 0;
+        audio.volume = 1; // Встановити гучність на максимум
+        audio.play();
+    }
+
+    function stopNoteWithoutSustain(audio) {
+        if (!sustainCheckbox.checked) {
+            let fadeOutInterval = setInterval(() => {
+                if (audio.volume > 0.1) {
+                    audio.volume -= 0.1;
+                } else {
+                    clearInterval(fadeOutInterval);
+                    audio.pause();
+                    audio.currentTime = 0;
+                    audio.volume = 1; // Відновити гучність для наступного відтворення
+                }
+            }, 50); // Інтервал для поступового зменшення гучності
+        }
+    }
 });
