87fa60306241785fe85cb276795f131800fe85c6
Dmytro Shvets <d.shvets@ukma.edu.ua>
2024-05-28 16:30:49 +0300

    connect db

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,28 @@
 from flask import Flask
-from secret import secret_key
+from pymongo import MongoClient
+from secret import secret_key, db_password
+
+client = MongoClient(
+    f'mongodb+srv://yenote05:{db_password}@yenote.epwf1qh.mongodb.net/?retryWrites=true&w=majority&appName=yeNote')
+yenote_db = client.yenote
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = secret_key
-
+    print(client.list_database_names())
+    print(yenote_db.list_collection_names())
+    # create_user()
     from .views import views
     app.register_blueprint(views, url_prefix='/')
 
     return app
+
+
+def create_user():
+    collection = yenote_db.user
+    test_doc = {
+        "name": "Yeva",
+        "surname": "Adam"
+    }
+    collection.insert_one(test_doc)
